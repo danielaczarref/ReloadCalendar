76d4ecb5ac164276bf690759036742e7817a9305
Daniela Carvalho <daniela.czarref@gmail.com>
2021-03-15 03:11:35 -0300

    D -HomePage- integrating lifestyle picture, defining new styles for home page and adding labels

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from '../../components/moleculas/Header';
 
-import * as SC from './styles'
+import * as SC from './styles';
+import * as S from '../../styles';
 
 export default function HomePage() {
 
@@ -9,6 +10,17 @@ export default function HomePage() {
   return (
     <SC.Container>
       <Header name="Daniela Carvalho" source={sourceImg} />
+      <SC.ContainerLifeStyle 
+        source={require('./assets/ilustrationLifestyle.png')}
+        accessibilityRole="image"
+        accessibilityLabel="Ilustração de pessoas passeando ao ar livre"
+      />
+      <SC.FooterHome>
+        <SC.RowOptions>
+          <S.FontLg color={S.colors.taupe_gray}>Timestamp feed</S.FontLg>
+          <S.FontLg>Activity feed</S.FontLg>
+        </SC.RowOptions>
+      </SC.FooterHome>
     </SC.Container>
   )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import * as S from '../../styles';
 
 export const Container = styled.View`
   flex: 1;
@@ -19,4 +20,19 @@ export const Input = styled.TextInput`
   marginBottom: 15px;
   marginHorizontal: 20px;
   fontSize: 16px;
+`;
+
+export const ContainerLifeStyle = styled.Image`
+  width: ${S.dimensions.fullWidth};
+`;
+
+export const FooterHome = styled.View`
+  background-color: ${S.colors.alabaster};
+  flex: 1;
+`;
+
+export const RowOptions = styled.View`
+  flex-direction: row;
+  margin: 8px 48px;
+  justify-content: space-between;
 `;
